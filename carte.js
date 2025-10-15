23352a6a3b8cd1b384c60acd7781afd3e41dac7e
doremilx <remilin77184@gmail.com>
2025-10-15 12:19:35 +0200

    Mise à jour carte

--- carte.js
+++ carte.js
@@ -1,11 +1,12 @@
 /* PARTIE CARTE */
-  var map = L.map('map').setView([48.866667,2.333333], 3.3);
+  var map = L.map('map').setView([48.866667,2.333333], 2.4);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 2,
+	maxZoom: 10,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 }).addTo(map);
-
 
 
 
@@ -14,7 +15,7 @@ fetch('data_pays_labubu.json')
   .then(data => {
     data.forEach((item) => {
       var marker = L.marker([item.latitude, item.longitude]).addTo(map); 
-      marker.bindPopup("<b>" + item.pays + "</b><br>I am a popup. <br>" + item.latitude + " " +item.longitude).openPopup();
+      marker.bindPopup("<b>" + item.pays + "</b><br>I am a popup. <br>" + item.latitude + " " +item.longitude);
     })
     });
 
