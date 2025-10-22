debfc8a5797b9ade91d1b79bc8427ea16d11f1ec
doremilx <remilin77184@gmail.com>
2025-10-22 18:57:24 +0200

    design graphique empile

--- graphique.js
+++ graphique.js
@@ -1,89 +1,3 @@
-/* Partie Graphique en barres */
-
-function dessineAxes(id, largeur, hauteur, epaisseur) {
-  document.querySelector(id).innerHTML += `<path class="axevertical" style="fill:black" d="M 0 0 L 0 ${hauteur} L ${epaisseur} ${hauteur} L ${epaisseur} 0"  />
-<path class="axehorizontal" style="fill:black;" d="M 0 ${hauteur} L ${largeur} ${hauteur} L ${largeur} ${hauteur - epaisseur} L 0 ${hauteur - epaisseur} " />`
-}
-
-function dessineBarre(id, numero, hauteur, largeur, ecart, couleur) {
-  const svg = document.querySelector(id)
-  const hauteursvg = svg.clientHeight;
-  svg.innerHTML += `<rect class="barre" style="fill:${couleur};" x="${numero * (20 + ecart)}" y="${100 - hauteur}" width="${largeur}" height="${hauteur}" data-value="${hauteur}"/>`
-  console.log(hauteursvg)
-  console.log(svg)
-}
-
-function dessineHistogramme(identifiantC, largeurH, hauteurH, tableau, largeurB, ecart, couleurB) {
-  document.querySelector('body').innerHTML += ` <svg class="histogramme" id="${identifiantC}" style="width: ${largeurH}px; height: ${hauteurH}px" viewBox="0 0 100 100"> 
- </svg>`
-  dessineAxes(identifiantC, largeurH, hauteurH, 2)
-  tableau.forEach(function (barreHauteur, numero) {
-    dessineBarre(identifiantC, numero, barreHauteur, largeurB, ecart, couleurB)
-  })
-
-/* document.querySelectorAll('.barre').forEach(function (element) {
-            element.style.transform = "scaleY(0)"
-            element.style.transformOrigin = "center bottom"
-            let animation = element.animate({ transform: "scaleY(1)" }, 2000)
-            animation.addEventListener('finish', function(){
-                element.style.transform = "scaleY(1)"
-            })
-        }) */}
-
-dessineHistogramme('#histogramme3', 100, 100, [10, 40, 20, 100, 50], 17, 1, 'red');
-/* Bon graphique pour les 5 objets tendances en France : labubu, molly, kiki, sonny angel, javascript */
-dessineHistogramme('#histogramme_marque', 100, 100, [35, 19, 12, 17, 17], 20, 1, 'blue');
-
-dessineHistogramme('#histogramme_test', 100, 100, [35, 19, 12, 17, 17], 20, 1, 'blue');
-
-
-dessineAxes('#histogramme2', 100, 100, 2)
-dessineBarre('#histogramme2', 0, 50, 10, 'red')
-
-dessineBarre('#histogramme2', 1, 20, 10, 'yellow')
-dessineBarre('#histogramme2', 2, 90, 10, 'green')
-
-document.querySelector("svg.histogramme").innerHTML += `
-
-<rect class="barre" style="fill:#0073ff;" 
-x="0" y="0" width="18" height="100" data-value="100"/>
-<rect class="barre" style="fill: #0073ff" 
-x="20" y="50" width="18" height="50" data-value="50"/> 
-<rect class="barre" style="fill: #0073ff" 
-x="40" y="10" width="18" height="90" data-value="90"/> 
-<rect class="barre" style="fill: #0073ff" 
-x="60" y="90" width="18" height="10" data-value="10"/>
-<rect class="barre" style="fill: #0073ff" 
-x="80" y="50" width="18" height="50" data-value="50"/> 
-<path class="axehorizontal" style="fill:black" d="M 0 0 L 0 100 L 2 100 L 2 0"  />
-
-<path class="axevertical" style="fill:black;" d="M 0 100 L 100 100 L 100 98 L 0 98 " />`
-
-
-
-document.querySelector("#histogramme_test").innerHTML += `
-
-<rect class="barre" style="fill:#ff0000ff;" 
-x="0" y="0" width="20" height="65" data-value="65"/>
-<rect class="barre" style="fill: #ff0000ff" 
-x="21" y="31" width="20" height="50" data-value="50"/> 
-<rect class="barre" style="fill: #ff0000ff" 
-x="42" y="10" width="20" height="90" data-value="90"/> 
-<rect class="barre" style="fill: #ff0000ff" 
-x="63" y="90" width="20" height="10" data-value="10"/>
-<rect class="barre" style="fill: #ff0000ff" 
-x="84" y="50" width="20" height="50" data-value="50"/> 
-<path class="axehorizontal" style="fill:black" d="M 0 0 L 0 100 L 2 100 L 2 0"  />
-
-<path class="axevertical" style="fill:black;" d="M 0 100 L 100 100 L 100 98 L 0 98 " />`
-
-document.querySelectorAll('.barre').forEach(function (lol) {
-  lol.addEventListener('mouseenter', function () {
-    document.querySelector('.infobulle').innerHTML = lol.getAttribute('data-value')
-
-  })
-});
-
 /* CODE POUR CHARTJS */
 
 const ctx = document.getElementById('myChart').getContext('2d');
@@ -94,27 +8,27 @@ const myChart = new Chart(ctx, {
     datasets: [
       {
         label: 'Labubu',
-        data: [1, 1, 1,2,3,3,3,22,40,95,85,45,33],
-        backgroundColor: 'rgba(255, 99, 132, 0.5)'
+        data: [(1/53*100), (1/52*100), (1/53*100),(2/39*100),(3/40*100),(3/37*100),(3/37*100),(22/54*100),(40/72*100),(95/133*100),(85/123*100),(45/76*100),(33/63*100)],
+        backgroundColor: '#B4C6FF'
       },
       {
         label: 'Sonny Angel',
-        data: [22, 22, 22,13,12,9,9,8,8,8,9,6,5],
-        backgroundColor: 'rgba(54, 162, 235, 0.5)'
+        data: [(22/53*100), (22/52*100), (22/53*100),(13/39*100),(12/40*100),(9/37*100),(9/37*100),(8/54*100),(8/72*100),(8/133*100),(9/123*100),(6/76*100),(5/63*100)],
+        backgroundColor: '#E99EA8'
       },
        {
         label: 'Figurine Pop',
-        data: [5, 8, 9,4,3,3,4,3,3,3,4,4,4],
-        backgroundColor: 'yellow'
+        data: [(5/53*100), (8/52*100), (9/53*100),(4/39*100),(3/40*100),(3/37*100),(4/37*100),(3/54*100),(3/72*100),(3/133*100),(4/123*100),(4/76*100),(4/63*100)],
+        backgroundColor: '#CBD7A8'
       },
        {
         label: 'Kiki',
-        data: [9, 8, 8,7,8,7,8,8,9,13,10,8,8],
-        backgroundColor: 'green'
+        data: [(9/53*100), (8/52*100), (8/53*100),(7/39*100),(8/40*100),(7/37*100),(8/37*100),(8/54*100),(9/72*100),(13/133*100),(10/123*100),(8/76*100),(8/63*100)],
+        backgroundColor: '#9D8EB8'
       }, {
         label: 'Molly',
-        data: [16, 13, 13,13,14,15,13,13,12,14,15,13,13],
-        backgroundColor: 'orange'
+        data: [(16/53*100), (13/52*100), (13/53*100),(13/39*100),(14/40*100),(15/37*100),(13/37*100),(13/54*100),(12/72*100),(14/133*100),(15/123*100),(13/76*100),(13/63*100)],
+        backgroundColor: '#584737'
       }
     ]
   },
